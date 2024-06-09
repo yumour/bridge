15593876427fadb6b72586d20eb5a4716a26de32
yumour <83502484+yumour@users.noreply.github.com>
2024-06-09 14:05:37 +0100

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ function AppFooter() {
   return (
     <div className="appFooter">
     
-      <p>   للتواصل معنا : djessrelmaarifa@gmail.com <br/> 0657857898 <br/>© 2023 All Rights Reserved</p>
+      <p>   للتواصل معنا : <br/>Email : djessrelmaarifa@gmail.com {'\t'} Phone : 0657857898 <br/>© 2023 All Rights Reserved</p>
     </div>
   );
 }
